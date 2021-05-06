a8a73477c6d41fc123c34e12f4085de71cc9e0aa
Catherine Aguilera <catherineaguilera13@gmail.com>
2021-05-06 13:31:35 -0400

    adding styles

---
+++ src/index.js
@@ -0,0 +1,21 @@
+//import { compareDesc, compareAsc, format } from 'date-fns';
+import {showingTaskForm} from './modules/addTask';
+//ordenar desde mas viejas compareDesc, desde las mas nuevas asc
+
+//console.log(format(new Date(), 'yyyy-MM-dd'))
+//=> '2014-02-11'
+
+/*const dates = [
+  new Date(1995, 6, 2),
+  new Date(1987, 1, 11),
+  new Date(1989, 6, 10),
+]*/
+//console.log(dates.sort(compareAsc))
+/*=> [
+  Wed Feb 11 1987 00:00:00,
+  Mon Jul 10 1989 00:00:00,
+  Sun Jul 02 1995 00:00:00
+]*/
+
+
+document.addEventListener('DOMContentLoaded', showingTaskForm)
